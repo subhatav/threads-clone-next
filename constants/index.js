f7a7fd3d9b54abd8066b9d685c928d84b4e8f326
Subhatav Dhali <subhatav@gmail.com>
2024-08-03 19:14:51 +0530

    Create the Top Bar and the Left Side Bar as `shared` components

---
+++ constants/index.js
@@ -0,0 +1,38 @@
+export const sidebarLinks = [
+  {
+    imgUrl: "/assets/home.svg",
+    route: "/", label: "Home"
+  },
+  {
+    imgUrl: "/assets/search.svg",
+    route: "/search", label: "Search"
+  },
+  {
+    imgUrl: "/assets/heart.svg",
+    route: "/activity", label: "Activity"
+  },
+  {
+    imgUrl: "/assets/create.svg",
+    route: "/create-thread", label: "Thread"
+  },
+  {
+    imgUrl: "/assets/groups.svg",
+    route: "/groups", label: "Groups"
+  },
+  {
+    imgUrl: "/assets/user.svg",
+    route: "/profile", label: "Profile"
+  },
+];
+
+export const profileTabs = [
+  { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
+  { value: "replies", label: "Replies", icon: "/assets/members.svg" },
+  { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" }
+];
+
+export const societyTabs = [
+  { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
+  { value: "members", label: "Members", icon: "/assets/members.svg" },
+  { value: "requests", label: "Requests", icon: "/assets/request.svg" }
+];
