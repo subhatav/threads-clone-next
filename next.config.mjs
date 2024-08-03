565b82829819ee1d504011e932c93bebb66a2a4a
Subhatav Dhali <subhatav@gmail.com>
2024-08-03 11:48:18 +0530

    Install the `uploadthing`, `svix`, `mongoose` and `clerk` dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,32 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  /* experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"]
+  }, */
   images: {
-    domains: [
-    "res.cloudinary.com",
-    "avatars.githubusercontent.com",
-    "lh3.googleusercontent.com"
-    ]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com"
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev"
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com"
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co"
+      },
+    ],
+    /* typescript: {
+      ignoreBuildErrors: true
+    } */
   }
 };
 
